e3b615d4b1c6bcc93b8617c10e69b1e80b918659
Alex Little <consult@alexlittle.net>
2023-06-24 10:26:15 +0300

    add lightbox

--- kollektiivi/signals.py
+++ kollektiivi/signals.py
@@ -13,12 +13,16 @@ site_tracker = Signal()
 def site_tracker_callback(sender, **kwargs):
     request = kwargs.get('request')
 
+    ignore_requests = ['/feed/', '.svg', '.ico']
     ip = request.META.get('REMOTE_ADDR', '0.0.0.0')
     agent = request.META.get('HTTP_USER_AGENT', 'unknown')
 
-    if search_crawler.is_search_crawler(agent) or \
-            request.user.is_authenticated:
+    if search_crawler.is_search_crawler(agent):
         return
+
+    for i in ignore_requests:
+        if request.build_absolute_uri().endswith(i):
+            return
 
     t = Tracker()
     t.url = request.build_absolute_uri()
