677943cde258180797cde807687f9e56d93da8fc
Alex Little <consult@alexlittle.net>
2023-06-25 16:18:41 +0300

    i18n urls

--- kollektiivi/urls.py
+++ kollektiivi/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+
+from django.conf.urls.i18n import i18n_patterns
+    
 from kollektiivi.views import HomeView, PageView, TagView, MembersView, MemberProfileView
 
 app_name = 'kollektiivi'
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('i18n/', include('django.conf.urls.i18n')),
     path('', HomeView.as_view(), name="home"),
     path('admin/', admin.site.urls),
@@ -15,7 +18,7 @@ urlpatterns = [
     path('uutiset/', include('blog.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('<str:slug>/', PageView.as_view(), name="page"),
-]
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
