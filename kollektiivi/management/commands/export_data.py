6e4cdff2e1c0d6bd815912e68c583276a92338c2
Alex Little <consult@alexlittle.net>
2023-07-04 21:59:41 +0300

    update footer etc

--- kollektiivi/management/commands/export_data.py
+++ kollektiivi/management/commands/export_data.py
@@ -8,23 +8,36 @@ class Command(BaseCommand):
 
     help = "Export models to json"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-m',
+            '--models',
+            dest='models',
+            help='Models to export',
+        )
+        
     def handle(self, *args, **options):
 
         base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
         fixture_root = os.path.join(base_dir, 'fixtures')
 
-        tags_file = os.path.join(fixture_root, 'tags.json')
-        with open(tags_file, 'w') as f:
-            call_command('dumpdata', "kollektiivi.tag", indent=4, stdout=f)
+        models = options['models']
+        if models == "all" or models == "tags":
+            tags_file = os.path.join(fixture_root, 'tags.json')
+            with open(tags_file, 'w') as f:
+                call_command('dumpdata', "kollektiivi.tag", indent=4, stdout=f)
 
-        pages_file = os.path.join(fixture_root, 'pages.json')
-        with open(pages_file, 'w') as f:
-            call_command('dumpdata', "kollektiivi.page", indent=4, stdout=f)
+        if models == "all" or models == "pages":
+            pages_file = os.path.join(fixture_root, 'pages.json')
+            with open(pages_file, 'w') as f:
+                call_command('dumpdata', "kollektiivi.page", indent=4, stdout=f)
 
-        members_file = os.path.join(fixture_root, 'members.json')
-        with open(members_file, 'w') as f:
-            call_command('dumpdata', "kollektiivi.member", indent=4, stdout=f)
+        if models == "all" or models == "members":
+            members_file = os.path.join(fixture_root, 'members.json')
+            with open(members_file, 'w') as f:
+                call_command('dumpdata', "kollektiivi.member", indent=4, stdout=f)
 
-        news_file = os.path.join(fixture_root, 'news.json')
-        with open(news_file, 'w') as f:
-            call_command('dumpdata', "blog.blog", indent=4, stdout=f)
+        if models == "all" or models == "news":
+            news_file = os.path.join(fixture_root, 'news.json')
+            with open(news_file, 'w') as f:
+                call_command('dumpdata', "blog.blog", indent=4, stdout=f)
--- kollektiivi/management/commands/import_data.py
+++ kollektiivi/management/commands/import_data.py
@@ -7,12 +7,25 @@ class Command(BaseCommand):
 
     help = "Import models from json"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-m',
+            '--models',
+            dest='models',
+            help='Models to import',
+        )
+
     def handle(self, *args, **options):
 
-        #call_command('loaddata', "tags")
+        models = options['models']
+        if models == "all" or models == "tags":
+            call_command('loaddata', "tags")
 
-        #call_command('loaddata', "pages")
+        if models == "all" or models == "pages":
+        call_command('loaddata', "pages")
 
-        #call_command('loaddata', "post")
+        if models == "all" or models == "news":
+        call_command('loaddata', "news")
 
+        if models == "all" or models == "members":
         call_command('loaddata', "members")
